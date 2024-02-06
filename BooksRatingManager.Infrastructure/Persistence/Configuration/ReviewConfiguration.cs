5434a8f8ea66b64b679b83a07c8cd4ca0320e058
Rodrigo Apolinário <124742288+RodrigoApol@users.noreply.github.com>
2024-02-06 14:07:17 -0300

    Update ReviewConfiguration.cs

--- BooksRatingManager.Infrastructure/Persistence/Configuration/ReviewConfiguration.cs
+++ BooksRatingManager.Infrastructure/Persistence/Configuration/ReviewConfiguration.cs
@@ -12,18 +12,6 @@ public class ReviewConfiguration : IEntityTypeConfiguration<Review>
             .HasKey(r => r.Id);
 
         builder
-            .HasOne(r => r.Book)
-            .WithMany()
-            .HasForeignKey(r => r.IdBook)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder
-            .HasOne(r => r.User)
-            .WithMany()
-            .HasForeignKey(r => r.IdUser)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder
             .Property(r => r.Description)
             .HasMaxLength(200)
             .HasColumnType("varchar(200)");
